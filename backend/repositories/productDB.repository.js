a452ab5c225834bb870c8d7edc5095de7133ea28
Alradenn <alradennibno@gmail.com>
2026-08-07 13:06:37 +0800

    feat(repository): add repository layer for products

---
+++ backend/repositories/productDB.repository.js
@@ -0,0 +1,52 @@
+import prisma from '../lib/prisma'
+
+export const productDB = {
+  findById(id) {
+    return prisma.product.findUnique({ where: { id } })
+  },
+  findMany() {
+    return prisma.product.findMany()
+  },
+  findAllFeatured() {
+    return prisma.product.findMany({ where: { isFeatured: true } })
+  },
+  findByCategory(category) {
+    return prisma.product.findMany({ where: { category } })
+  },
+  createProduct(data) {
+    return prisma.product.create({ data })
+  },
+  updateProductFeature(id, isFeatured) {
+    return prisma.product.update({
+      where: { id, isFeatured },
+      data: { isFeatured: !isFeatured },
+    })
+  },
+  updateProductData(id, newData) {
+    return prisma.product.update({
+      where: { id },
+      data: newData,
+    })
+  },
+  deleteProduct(id) {
+    return prisma.product.delete({ where: { id } })
+  },
+  findRecommendations(currentProductId, category) {
+    return prisma.product.findMany({
+      where: {
+        id: { not: currentProductId }, // Exclude current product
+        OR: [
+          { category }, // Condition 1
+          { isFeatured: true }, // Condition 2
+        ],
+      },
+      select: {
+        id: true,
+        title: true,
+        price: true,
+        category: true,
+        isFeatured: true,
+      },
+    })
+  },
+}
